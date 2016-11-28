d3edb4153b58d672ac7da24e9e1c2f028ea59e41
haileyanderton@gmail.com <hka122095!>
2016-11-27 23:09:27 -0500

    project week 1

--- js/women.js
+++ js/women.js
@@ -3,9 +3,11 @@ $(document).ready(function() {
   $('.womenInfo').hide();
 
   $('.women').click(function() {
-    $('#radio').click( function() {
-            Popup();
+    $('#radio').click(function() {
+      Popup();
   });
+
+  // planning to make the '.women' divs responsive/interractive so that the viewer is aware that they can click on the divs to recieve more information. 
 
     // function to hide all divs
     function hideAll() {
@@ -24,10 +26,10 @@ $(document).ready(function() {
 
   });
 
+//trying to figure out how to make a switch statement that applies/works in place of the popUp function, so that the correct popup will fade in for the corresponding topics.
+
   function Popup() {
-             $('.womenInfo').fadeIn("fast");
-             $("#container").css({
-             });
+         $('.womenInfo').fadeIn("fast");
          }
     });
 
